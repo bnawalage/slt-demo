5901b98f3688fb0c3bb78ef0ec1ebb4b778576fd
gayandissanayake <pdngayan@gmail.com>
2022-05-26 13:04:28 +0530

    gayan changes 2

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -4,8 +4,14 @@ public class Test {
     public static void main(String[] args) {
         System.out.println();
         System.out.println();
+        System.out.println("Gayan1");
+        System.out.println("Gayan2");
         System.out.println();
         System.out.println();
-        System.out.println("Gayan");
+        System.out.println("Gayan3");
+        System.out.println("Gayan4");
+        System.out.println("Gayan5");
+        System.out.println("Gayan6");
+        System.out.println("Gayan7");
     }
 }
